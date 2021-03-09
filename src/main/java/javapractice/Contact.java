eddd14953357360331f1b9f61e0d5f0f86db0ddf
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-03-09 12:31:51 +0530

    [Sankar]ADDED:First commit

---
+++ src/main/java/javapractice/Contact.java
@@ -0,0 +1,7 @@
+package javapractice;
+
+public class Contact {
+    public static void main(String[] args) {
+        System.out.println("---------WELCOME---------");
+    }
+}
---
+++ src/test/java/javapractice/BookTest.java
@@ -0,0 +1,10 @@
+package javapractice;
+
+import org.junit.jupiter.api.Test;
+
+public class BookTest {
+    @Test
+    public void startProgram() {
+        Contact contact = new Contact();
+    }
+}
